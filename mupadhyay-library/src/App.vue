60ab989f6ee9f6995a4488d607d1000a4dd05110
miti1800 <mupa0005@student.monash.edu>
2025-08-12 20:13:59 +1000

    completed all activities of task3.1

--- mupadhyay-library/src/App.vue
+++ mupadhyay-library/src/App.vue
@@ -2,21 +2,12 @@
 import HelloWorld from './components/HelloWorld.vue'
 import JSON from './components/JSON.vue';
 import TheWelcome from './components/TheWelcome.vue'
+import Form from './components/Form.vue';
 </script>
 
 <template>
-  <JSON />
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main> -->
+  <!-- <JSON /> -->
+   <Form />
 </template>
 
 <style scoped>
