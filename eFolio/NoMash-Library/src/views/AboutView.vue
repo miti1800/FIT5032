a498e829573e4be6f41679fe160f9285a248fcd1
miti1800 <mupa0005@student.monash.edu>
2025-09-05 19:58:30 +1000

    efolio task 5.2

---
+++ eFolio/NoMash-Library/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="d-flex justify-content-center mt-5">
+        <div class="about d-flex flex-column align-items-center">
+            <h1>About Our Library</h1>
+            <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// No script needed for now
+</script>
+
+<style>
+.about {
+    max-width: 750px;
+}
+</style>
