0ac789db6611e7c558d848a789b2af9267a1641f
miti1800 <mupa0005@student.monash.edu>
2025-09-14 19:50:44 +1000

    modified the pagenotfound component

--- assignment/src/views/PageNotFoundView.vue
+++ assignment/src/views/PageNotFoundView.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="text-center py-5">
-    <i class="bi bi-emoji-frown-fill fs-1"></i>
-    <h1 class="display-4">404</h1>
-    <p class="primary-color">Page not found</p>
-    <router-link to="/" class="primary-color">Go back home</router-link>
+  <div class="py-5 cover-gradient min-vh-100 d-flex flex-column align-items-center justify-content-center secondary-color">
+    <i class="bi bi-emoji-frown-fill display-2 secondary-color"></i>
+    <h1 class="display-2 fw-medium">Error: 404</h1>
+    <h6 class="display-6 mb-4">Page not found !!</h6>
+    <router-link to="/" class="btn primary-btn px-4 fs-4">Go back home</router-link>
   </div>
 </template>
 
-<script setup>
-</script>
+<style scoped>
+.cover-gradient {
+    background: linear-gradient(30deg, #f3f4f6 0%, #284B63 100%);
+}
+</style>
