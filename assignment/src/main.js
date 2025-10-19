f8ab8038a27a192b48da309c1146416d737ab229
miti1800 <mupa0005@student.monash.edu>
2025-10-19 21:41:34 +1100

    fixed issue of firebase config

--- assignment/src/main.js
+++ assignment/src/main.js
@@ -3,7 +3,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import './firebaseconfig'
+import './firebaseConfig'
 import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
